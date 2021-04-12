45c701a31a380a9f45b4853d41b76f9b51626a2c
bachtn002 <bachtn002@gmail.com>
2021-04-13 00:14:04 +0700

    GG

--- CakeShop.BackendAPI/Startup.cs
+++ CakeShop.BackendAPI/Startup.cs
@@ -34,7 +34,7 @@ namespace CakeShop.BackendAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews().AddFluentValidation();
+            services.AddControllersWithViews();
             services.AddDbContext<CakeShopDbContext>(options =>
         options.UseSqlServer(Configuration.GetConnectionString("CakeShopDb")));
             services.AddTransient<IProductService, ProductService>();
